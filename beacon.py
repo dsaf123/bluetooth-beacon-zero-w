17cb0366525be6e8c07758f068502a37f150b42d
Andrew Henningsen <henningsenandrew@gmail.com>
2020-05-12 15:09:05 -0500

    Added BT Address

--- beacon.py
+++ beacon.py
@@ -6,7 +6,7 @@ import threading
 import sys
 
 # List of bluetooth addresses to scan
-BT_ADDR_LIST = []
+BT_ADDR_LIST = ["A0:10:81:A2:D8:7C"]
 DAILY = True  # Set to True to invoke callback only once per day per address
 DEBUG = True  # Set to True to print out debug messages
 THRESHOLD = (-10, 10)
